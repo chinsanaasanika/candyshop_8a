13ac95a385045c95794c2ccabfb186642a16b94a
chinsanaasanika <126443612+chinsanaasanika@users.noreply.github.com>
2023-03-06 07:06:54 +0000

    ungunuudiin uurchinsn intro hiisn logo,name 2t tusd n parent ugsn login singin bas tusd n parent ugsn

--- haebom/uu.js
+++
@@ -1,7 +0,0 @@
-let une   = 2500;
-function CandyShop() {
-    let torol = document.getElementById("torol").value;
-    let sh = document.getElementById("shirhg").value;
-    let tulbur = une * sh;
-    document.getElementById("tulbur").innerHTML = tulbur;
-}
